9eade0e9a43c9a6d200bb20a256c0a0e26ab994d
atikur28 <atikur45683@gmail.com>
2023-10-18 21:56:38 +0600

    products update and delete completed

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -33,10 +33,44 @@ async function run() {
         res.send(result);
     })
 
+    app.get('/products/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await productsCollection.findOne(query);
+      res.send(result);
+    })
+
     app.post('/products', async (req, res) => {
         const newProduct = req.body;
         const result = await productsCollection.insertOne(newProduct);
         res.send(result);
+    })
+
+    app.put('/products/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)};
+      const options = {upsert: true};
+      const updatedProduct = req.body;
+      const product = {
+        $set: {
+          name: updatedProduct.name,
+          brand: updatedProduct.brand,
+          type: updatedProduct.type,
+          rating: updatedProduct.rating,
+          price: updatedProduct.price,
+          image: updatedProduct.image,
+          description: updatedProduct.description
+        }
+      };
+      const result = await productsCollection.updateOne(filter, product, options);
+      res.send(result);
+    })
+
+    app.delete('/products/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await productsCollection.deleteOne(query);
+      res.send(result);
     })
 
     // Send a ping to confirm a successful connection
